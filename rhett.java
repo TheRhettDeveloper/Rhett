261bb89150398530c6cfb0664f11faf4b5a9b91d
TheRhettDeveloper <eliabernathyloot@gmail.com>
2023-03-20 16:45:27 -0400

    Create rhett.java

---
+++ rhett.java
@@ -0,0 +1,18 @@
+
+class rhett{
+
+    public static void main (String args[]){
+        int strength = 10;
+        int dexterity = 10;
+        int constitution = 10;
+        int intelligence = 10;
+        int wisdom = 10;
+        int charisma =10;
+        System.out.println("Strength = " +strength);
+        System.out.println("Dexterity = " +dexterity);
+        System.out.println("Constitution = " +constitution);
+        System.out.println("Intelligence = " +intelligence);
+        System.out.println("Wisdom = " +wisdom);
+        System.out.println("Charisma = " +charisma);
+    }
+}
